b9e37e7563743b345cf8e5203cfb91956d3f65b5
Your Name <you@example.com>
2026-04-14 22:39:48 +0000

    chore: cleanup scratch scripts and optimize repo space

--- server/check_user.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const user = await prisma.user.findUnique({ where: { email: 'md@nexus.com' } });
-  console.log(JSON.stringify(user, null, 2));
-}
-main();
